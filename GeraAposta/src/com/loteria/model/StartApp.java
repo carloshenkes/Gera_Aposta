0c93e26739353e8a0905c095ef383d36621f9fa9
daniel <daniel@daniel-Aspire-E5-571G>
2016-06-04 13:24:46 -0300

    Deu Certo?

--- GeraAposta/src/com/loteria/model/StartApp.java
+++ GeraAposta/src/com/loteria/model/StartApp.java
@@ -4,6 +4,7 @@ import com.loteria.gui.PrincipalFrame;
 
 public class StartApp {
     //DEU
+    //DEU
     public static void main(String[] args){
         PrincipalFrame f = new PrincipalFrame();
         f.setVisible(true);
